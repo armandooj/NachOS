5fc2e8751beb38941a37738ac3a48d804a7e03d2
Armando Ochoa Jiménez <armando.o.j@gmail.com>
2015-01-13 17:20:42 +0100

    Using yield() makes the newThread run

--- code/userprog/userthread.cc
+++ code/userprog/userthread.cc
@@ -11,22 +11,65 @@
 #include "system.h"
 #include "userthread.h"
 
+/*
+Initialises backups of registers of a new copy of the MIPS interpreter in the same way as the primitive interpreter 
+(Machine::InitRegisters\ and \ |Machine::RestoreState\ functions), and starts the interpreter (\| Machine::Run).
+*/
+static void StartUserThread(int f) {
+  // call f ??? 
+  printf("StartUserThread\n");
+  DEBUG('a', "Hola");
+
+  //currentThread->space->InitRegisters();
+  int i;
+  for (i = 0; i < NumTotalRegs; i++)
+
+
+    machine->WriteRegister (i, 0);
+
+  //machine->WriteRegister (4, f);
+  // Initial program counter -- must be location of "Start"
+  machine->WriteRegister (PCReg, f);
+  // Need to also tell MIPS where next instruction is, because
+  // of branch delay possibility
+  machine->WriteRegister (NextPCReg, f + 4);
+
+
+  // Set the stack register
+  //machine->WriteRegister (StackReg, 0); ??  3 paginas
+
+  // machine->WriteRegister (RetAddrReg, 12);
+    //   int lol;
+    
+    // int lol = UserStackSize ?? / MaxThread * thread_id + 16;
+    // machine->WriteRegister (StackReg, (numPages*PageSize) - lol);
+}
+
 int do_UserThreadCreate(int f, int arg) {
 
-    /*
-    Thread *t = new Thread("New Thread");
-    
-    t->Fork(  , f);
-    
-    
-    long t; 
-    
-    t = combine(f, avg);
-    
-    thread -> Fork(StartUserThread, &t);
-    
-    */
-    return 0;
+  printf("do_UserThreadCreate\n");
+
+  Thread *newThread = new Thread("New Thread");
+  currentThread->setStatus (READY);
+  
+  newThread->Fork(StartUserThread, f);
+  scheduler->Print();
+ 
+  currentThread->Yield();
+  scheduler->Print();
+
+  // Thread *t = new Thread(  );
+  
+  // t->Fork(  , f);
+  
+  
+  // long t; 
+  
+  // t = combine(f, avg);
+  
+  // thread -> Fork(StartUserThread, &t);
+  
+  return 0;
 }
 
 #endif
