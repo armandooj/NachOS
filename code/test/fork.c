aa1ba89acb47ddcc7bfbca4ccf80fc8d46dc6826
Armando Ochoa Jiménez <armando.o.j@gmail.com>
2015-01-23 10:51:42 +0100

    Adding the real test for ForkExec
    Conflicts:
    	code/test/fork.c
    	code/test/userpages0.c
    	code/test/userpages1.c

--- code/test/fork.c
+++ code/test/fork.c
@@ -1,10 +1,10 @@
 #include "syscall.h"
 
 int main() {
-  // ForkExec("userpages0");
-  PutChar('b');
-  // ForkExec("userpages1"); 
-  PutChar('c');
-  //ForkExec("putstring");
+
+  ForkExec("userpages0");
+  // PutChar('b');
+  ForkExec("userpages1"); 
+  // PutChar('c');
   return 0;
 }
--- code/test/userpages1.c
+++ code/test/userpages1.c
@@ -1,24 +1,23 @@
 #include "syscall.h"
+#define THIS "xxx"
+#define THAT "yyy"
 
-void print(void *c)
-{
-    int i;
-    for (i = 0; i < 1; i++) {
+const int N = 10; // Choose it large enough!
+
+void my_puts(void *c) {
+    char *p; 
+    for (p = c; *p != '\0'; p++) 
         PutChar(* ((char *) c));
-    }
+}
+
+void f(void *s) {
+    int i; 
+    for (i = 0; i < N; i++) 
+        my_puts((char *) s);
 }
 
 int main() {
-
-    int t1;
-    //int t2;
-    char ch1 = 'b';
-    //char ch2 = 'y';
-    char *c1 = &ch1;
-    //char *c2 = &ch2;
-    t1 = UserThreadCreate(print, (void *) c1);
-    //t2 = UserThreadCreate(print, (void *) c2);
-    
+    UserThreadCreate(f, (void *) THIS);
+    f((void*) THAT);
     return 0;
 }
-
