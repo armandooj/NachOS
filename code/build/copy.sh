ef6da91ae185eece590ff120fc3667e9b3ed6c90
Mina Khalil <minakhalil1991@gmail.com>
2015-01-30 01:16:49 +0100

    changes

--- code/build/copy.sh
+++ code/build/copy.sh
@@ -10,10 +10,10 @@
 ./nachos-final -cp fork fork-test
 ./nachos-final -cp userpages0 userpages0-test
 ./nachos-final -cp join_multithread join_multithread-test
-./nachos-final -cp multopen multopen 
-./nachos-final -cp mulpopen mulpopen
-./nachos-final -cp fork1 fork1
-./nachos-final -cp fork2 fork2
-./nachos-final -cp conopen conopen
+./nachos-final -cp multopen multopen-test 
+./nachos-final -cp mulpopen mulpopen-test
+./nachos-final -cp fork1 fork1-test
+./nachos-final -cp fork2 fork2-test
+./nachos-final -cp conopen conopen-test
 
 clear
